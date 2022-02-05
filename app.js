85a4f40ebc6775641a14bd7620ffe572df514089
Sergey Kryvets <s.kryvets@gmail.com>
2022-02-05 12:56:46 -0600

    improvements/fixes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.json({
+  "message": "Hello World!"
+}))
+
 app.listen(3000, () => console.log('Server ready'))
